b2b104259da43ecf222a702f6107ba49a03ada14
prats011 <jhaveripratham2006@gmail.com>
2025-08-21 09:09:41 +0700

    +successApp

--- src/router.js
+++ src/router.js
@@ -4,12 +4,15 @@ import HomeApp from '@/HomeApp.vue'
 import ArrivalCard from '@/views/ArrivalCardApp.vue'
 import TripAccomadation from './views/TripAccomadationApp.vue'
 import HealthDeclaration from './views/HealthDeclarationApp.vue'
+import SuccessApp from './views/SuccessApp.vue'
 
 const routes = [
   { path: '/', component: HomeApp },
   { path: '/arrival-card/personal-information', component: ArrivalCard },
   { path: '/arrival-card/trip-&-accomodation-information', component: TripAccomadation },
-  { path: '/arrival-card/health-declaration', component: HealthDeclaration }
+  { path: '/arrival-card/health-declaration', component: HealthDeclaration },
+  { path: '/arrival-card/success', component: SuccessApp }
+  
 
 ]
 
